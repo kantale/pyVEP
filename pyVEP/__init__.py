5c449021155812af1ddc171fd31f927d91f6d972
kantale <alexandros.kanterakis@gmail.com>
2015-12-14 13:16:14 +0200

    emulate curl call

---
+++ pyVEP/__init__.py
@@ -0,0 +1,54 @@
+
+import urllib
+import logging
+import requests
+
+
+'''
+Example curl command:
+curl 'http://rest.ensembl.org/vep/Homo_sapiens/hgvs/9%3Ag.22125504G%3EC' \
+-H 'Host: rest.ensembl.org' \
+-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:42.0) Gecko/20100101 Firefox/42.0' \
+-H 'Accept: application/json, text/javascript, */*; q=0.01' \
+-H 'Accept-Language: en-US,en;q=0.5' \
+--compressed \
+-H 'Referer: http://asia.ensembl.org/Tools/VEP?redirect=no' \
+-H 'Origin: http://asia.ensembl.org' \
+-H 'Connection: keep-alive'
+'''
+
+def VEP(variant):
+	
+	url_pattern = 'http://rest.ensembl.org/vep/Homo_sapiens/hgvs/{variant}'
+
+	headers = {
+		'Host': 'rest.ensembl.org',
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:42.0) Gecko/20100101 Firefox/42.0',
+		'Accept': 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Referer': 'http://asia.ensembl.org/Tools/VEP?redirect=no',
+		'Origin': 'http://asia.ensembl.org',
+		'Connection': 'keep-alive',
+	}
+
+	variant_quoted = urllib.quote(variant)
+	url = url_pattern.format(variant=variant_quoted)
+
+	logging.debug('URL: %s' % (url))
+	r = requests.get(url, headers=headers)
+	json_text = r.json()
+
+	return json_text
+
+def test():
+	import json
+	
+	logging.basicConfig(level=logging.DEBUG)
+
+	def try_variant(variant):
+		logging.info('Trying variant: %s' % (variant))
+		r = VEP(variant)
+		logging.info('Result: %s' % json.dumps(r, indent=4))
+
+	try_variant('9:g.22125504G>C')
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+
+
+setup(name='pyVEP',
+      version='0.0.1',
+      description='Python interface to Variant Effect Predictor',
+      url='https://github.com/kantale/pyVEP',
+      author='Alexandros Kanterakis',
+      author_email='alexandros.kanterakis@gmail.com',
+      license='MIT',
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers 
+      classifiers=[
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+      install_requires=[
+            'requests',
+      ],
+      # http://stackoverflow.com/questions/3472430/how-can-i-make-setuptools-install-a-package-thats-not-on-pypi 
+      # dependency_links=['https://github.com/counsyl/hgvs/tarball/master#egg=pyhgvs-2.0.0',],
+      packages=['pyVEP'],
+)
+
+
